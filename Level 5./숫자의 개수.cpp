d0617c626ce8c9601a16d19d92de0794893a5cd9
Sumin Park <82919411+elephant97@users.noreply.github.com>
2021-11-10 00:31:42 +0900

    Update 숫자의 개수.cpp

--- Level 5./숫자의 개수.cpp
+++ Level 5./숫자의 개수.cpp
@@ -1,38 +1,31 @@
-#include<iostream>
 using namespace std;
 
 int main()
-{
-	double *arr = NULL;
-	int count = 0; double average = 0.00;
-	double max = 0; int i = 0;
+{ 
+	int arr[3] = { 0, };
+	int return_arr[10] = { 0, };
+	int cal = 0; int num = 0; int x = 10;
 
-	cin >> count;
-	
-	arr = new double[count];
-
-	for (i = 0; i < count; i++)
+	for (int i = 0; i < 3; i++)
 	{
 		cin >> arr[i];
 		if (i == 0)
 		{
-			max = arr[i];
+			cal = arr[i];
+			continue;
 		}
-		
-		max = arr[i] > max ? arr[i] : max;	
+		cal *= arr[i];
 	}
 
-	for (i = 0; i < count; i++)
+	while (cal != 0)
 	{
-		arr[i] = arr[i] / max * 100.0;
+		num = cal % x;
+		cal /= x;
+		return_arr[num]++;
 	}
 
-	average = arr[0];
-	for (i = 1; i < count; i++)
+	for (int i = 0; i < 10; i++)
 	{
-		average += arr[i];
+		cout << return_arr[i] <<"\n";
 	}
-
-	cout << average / (double)count;
-	delete []arr;
 }
