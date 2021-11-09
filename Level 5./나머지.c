a66d3dc52e32818f72787fb54dff83ae5e882093
Sumin Park <82919411+elephant97@users.noreply.github.com>
2021-11-10 00:00:48 +0900

    Create 나머지.c

---
+++ Level 5./나머지.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int main()
+{
+	int arr[10] = { 0, };
+	int x = 0; int i = 0;
+	int count = 0;
+	int last_overlap_flag = 0; //last array overlap true or false
+
+	for (i = 0; i < 10; i++)
+	{
+		scanf("%d",&arr[i]);
+		arr[i] %= 42;
+	}
+
+
+	for (i = 0; i < 10; i++)
+	{
+		for (x = i + 1; x < 10; x++)
+		{
+			if (arr[x] == arr[i])
+			{
+				if (x == 9 && last_overlap_flag != 1)
+				{
+					last_overlap_flag == 1;
+				}
+				break;
+			}
+			
+			if (x == 9)
+			{
+				count++;
+			}
+		}
+		
+	}
+
+	if (last_overlap_flag != 1)
+	{
+		count++;
+	}
+	printf("%d",count);
+	
+}
