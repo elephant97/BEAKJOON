3585d6e9cdffb23223226ab9cfaa6f8e84d15869
Sumin Park <82919411+elephant97@users.noreply.github.com>
2021-11-11 23:31:52 +0900

    Update 평균은넘겠지.cpp

--- Level 5./평균은넘겠지.cpp
+++ Level 5./평균은넘겠지.cpp
@@ -5,27 +5,24 @@ int main()
 {
     int in_cnt = 0, person_cnt = 0, i = 0, x = 0, up_cnt = 0;
     double avg = 0.00;
-    string OX_str = "";
     double* score = NULL;
-    double* percent_upscore = NULL;
+    double percent_upscore = NULL;
 
     cin >> in_cnt;
-    percent_upscore = new double[in_cnt] {0, };
 
     for (i = 0; i < in_cnt; i++)
     {
         avg = 0;
-        cout << i;
         cin >> person_cnt;
         score = new double[person_cnt] {0, };
         for (x = 0; x < person_cnt; x++)
-        {   
-            cin >> score[i];
-            avg += score[i];
+        {
+            cin >> score[x];
+            avg += (double)score[x];
         }
 
         up_cnt = 0;
-        avg /= (double)person_cnt;
+        avg /= person_cnt;
         for (x = 0; x < person_cnt; x++)
         {
             if (score[x] > avg)
@@ -33,15 +30,10 @@ int main()
                 up_cnt++;
             }
         }
-        percent_upscore[i] = (double)((double)up_cnt / (double)person_cnt * (double)100);
-        cout << percent_upscore[i] << "% \n";
-        delete[]score;
-    }
 
-    for (i = 0; i < in_cnt; i++)
-    {
-        cout << percent_upscore[i] << "%\n";
+        cout.precision(3);
+        percent_upscore = (double)up_cnt / person_cnt * 100;
+        cout << fixed << percent_upscore << "%\n";
+        delete[]score; 
     }
-
-    delete []percent_upscore;
 }
