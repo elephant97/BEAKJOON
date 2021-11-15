479ee273a87f29a99cf11691cf34da6b65630665
Sumin Park <82919411+elephant97@users.noreply.github.com>
2021-11-15 23:42:10 +0900

    Create 셀프넘버.cpp

---
+++ Level 6./셀프넘버.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int d(int n)
+{
+	int i = 0;
+	int sum = n;
+	while (n != 0)
+	{
+		i = n % 10;
+		n /= 10;
+		sum += i;
+	}
+
+	return sum;
+}
+
+int main()
+{
+	bool answer[10001] = { false, };
+	int ret = 0;
+	for (int i = 1; i <= 10000; i++)
+	{	
+		ret = d(i);
+		if(ret <= 10000 && i != ret)
+		{
+			answer[ret] = true;
+		}
+	}
+
+	for (int i = 1; i <= 10000; i++)
+	{
+		if (answer[i] == false)
+		{
+			cout << i << "\n";
+		}
+	}
+	
+}
