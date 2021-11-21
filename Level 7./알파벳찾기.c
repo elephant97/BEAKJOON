609f2b97494e406a6f72a0eea772b2f2f981e206
Sumin Park <82919411+elephant97@users.noreply.github.com>
2021-11-22 00:55:26 +0900

    Create 알파벳찾기.c

---
+++ Level 7./알파벳찾기.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char str[100];
+	int i = 0, x = 0;
+
+	scanf("%s", str);
+	for (i = 97; i < 123; i++)
+	{
+		x = 0;
+		while (x < strlen(str))
+		{
+			if (str[x] == (char)i)
+			{
+				break;
+			}
+
+			x++;
+		}
+
+		if (str[x] == (char)i)	printf("%d ", x);
+		else	printf("-1 ");
+		
+	}
+
+}
