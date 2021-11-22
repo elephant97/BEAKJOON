d4ea2759163dacfd3825befa1ea31fdf8e827cb1
Sumin Park <82919411+elephant97@users.noreply.github.com>
2021-11-22 23:55:41 +0900

    Create 문자열반복.cpp

---
+++ Level 7./문자열반복.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int cnt = 0, i = 0, repeat_cnt = 0, x = 0, y = 0;
+	string str;
+	
+	
+	cin >> cnt;
+	for (i = 0; i < cnt; i++)
+	{
+		cin >> repeat_cnt;
+		cin >> str;
+
+		for (x = 0; x < str.length(); x++)
+		{
+			for (y = 0; y < repeat_cnt; y++)
+			{
+				cout << str[x];
+			}
+		}
+		cout << "\n";
+	}
+
+}
