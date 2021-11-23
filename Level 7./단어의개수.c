95c47b34aab85e67c3bb3c5c942752e74e75f6d8
Sumin Park <82919411+elephant97@users.noreply.github.com>
2021-11-24 00:55:45 +0900

    Create 단어의개수.c

---
+++ Level 7./단어의개수.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+
+    char str[1000000 + 1] = { 0, };
+    int i = 0, cnt = 0, length = 0;
+
+    scanf("%[^\n]",str); //개행 입력시 문자받기를 종료
+    length = strlen(str);
+    if (length == 0)
+    {
+        printf("%d", cnt);
+        return 0;
+    }
+
+    cnt++;
+
+    while (i != length)
+    {
+        if (str[i] == ' ' && i != 0 && i != (length -1))
+        {
+            cnt++;
+        }
+        i++;
+    }
+
+    printf("%d", cnt);
+}
