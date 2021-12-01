8c24f2a91b440e37c531507e41a9b95ef23a55e4
Sumin Park <82919411+elephant97@users.noreply.github.com>
2021-12-02 00:13:27 +0900

    백준 2839번 문제

---
+++ Level 8./설탕배달(미완성).cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int gram = 0, temp = 0, i = 0;
+	int sugar = 0;
+	cin >> gram;
+
+
+	if ((temp = gram/5) > 0)
+	{
+		while (temp >= 0)
+		{
+			if ((gram - (temp * 5)) % 3 == 0)
+			{
+				sugar = temp + (gram - (temp * 5)) / 3;
+				break;
+			}
+			temp--;
+		}
+		if (temp == 0) sugar = -1;
+	}
+	else if (gram % 3 == 0)
+	{
+		sugar = gram / 3;
+	}
+	else
+	{
+		sugar = -1;
+	}
+		
+	
+
+	cout << sugar;
+	return 0;
+}
