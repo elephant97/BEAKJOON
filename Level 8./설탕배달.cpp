e0f7bd39321bbf34c1793267b67277c4b2790138
Sumin Park <82919411+elephant97@users.noreply.github.com>
2021-12-02 22:56:48 +0900

    백준 2839번

--- Level 8./설탕배달.cpp
+++ Level 8./설탕배달.cpp
@@ -3,36 +3,25 @@ using namespace std;
 
 int main()
 {
-	int gram = 0, temp = 0, i = 0;
-	int sugar = 0;
-	cin >> gram;
+    int gram = 0, temp = 0, i = 0;
+    int sugar = 0;
+    cin >> gram;
+
+    temp = gram / 5;
+
+    while (temp >= 0)
+    {
+        if ((gram - (temp * 5)) % 3 == 0) //5의 최대치로 나누고 3의 최소치로 나누었을 때
+        {
+            sugar = temp + (gram - (temp * 5)) / 3;
+            cout << sugar << "\n";
+            break;
+        }
+        temp--;
+    }
+    if (temp < 0) sugar = -1; //5으로도 3으로도 안 나눠지는 경우
 
 
-	if ((temp = gram / 5) > 0)
-	{
-		while (temp >= 0)
-		{
-			if ((gram - (temp * 5)) % 3 == 0)
-			{
-				sugar = temp + (gram - (temp * 5)) / 3;
-				cout << sugar << "\n";
-				break;
-			}
-			temp--;
-		}
-		if (temp < 0) sugar = -1;
-	}
-	else if (gram % 3 == 0)
-	{
-		sugar = gram / 3;
-	}
-	else
-	{
-		sugar = -1;
-	}
-
-
-
-	cout << sugar;
-	return 0;
+    cout << sugar;
+    return 0;
 }
